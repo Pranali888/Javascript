114fe2360976903bbd1ef6b5a36ce8d061baf04b
Pranalee <121343645+Pranali888@users.noreply.github.com>
2026-02-05 05:44:31 +0000

    datatypes summary

--- conversions.js
+++ conversions.js
@@ -1,10 +1,19 @@
 let score = "1000"
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let ValueInNumber = Number(score);
-console.log(ValueInNumber, typeof(ValueInNumber));
+//console.log(ValueInNumber, typeof(ValueInNumber));
 
 let someNumber = 346
 let stringNumber = String(someNumber)
-console.log(typeof(stringNumber), stringNumber);
+//console.log(typeof(stringNumber), stringNumber);
+
+//********OPS********
+
+
+let x = 4
+let negVal = -x
+//console.log(negVal);
+//console.log(2**4);
+
